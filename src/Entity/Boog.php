6219e82581c8aa12d1bb78edfdefc8c392b83f98
Alexander <alexander.advanced@gmail.com>
2020-04-07 19:25:32 +0300

    new strategy for entity field creation

--- src/Entity/Boog.php
+++ src/Entity/Boog.php
@@ -17,8 +17,42 @@ class Boog
 	 */
 	private $id;
 	
+	/**
+	* @ORM\Column(type="string", length=255, nullable=true)
+	*/
+	private $title;
+	
+	/**
+	* @ORM\Column(type="string", length=255, nullable=true)
+	*/
+	private $about;
+	
 	public function getId()
 	{
 		return $this->id;
+	}
+	
+	public function getTitle()
+	{
+		return $this->title;
+	}
+	
+	public function setTitle($value): self
+	{
+		$this->title = $value;
+	
+		return $this;
+	}
+	
+	public function getAbout()
+	{
+		return $this->about;
+	}
+	
+	public function setAbout($value): self
+	{
+		$this->about = $value;
+	
+		return $this;
 	}
 }
